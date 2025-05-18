76d55e55e2c3695e6c34648bc1c5f411d6d34d60
NixonR <nixonramosdue@gmail.com>
2025-05-18 10:13:13 -0500

    qiueoiuq

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace EjercicioPrueba
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
     }
 }
